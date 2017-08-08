053f893fe16cc7023b5937a75a99cc7a5bf7bc4b
Artem Zakharchenko <kettanaito@gmail.com>
2017-08-08 12:52:42 +0200

    Empty the inner state for further accumulation

--- src/index.js
+++ src/index.js
@@ -2,21 +2,29 @@ const EventPool = (customOptions) => {
   const options = Object.assign({}, {
     events: '',
     callback: () => ({}),
-    timeout: 500,
+    timeout: 15,
   }, customOptions);
 
   const { events } = options;
 
   const eventsList = Array.isArray(events) ? events : [events];
-  const pool = [];
-  const hasTimerStarted = false;
+  let pool = [];
+  let hasTimerStarted = false;
 
   eventsList.forEach(eventName => document.addEventListener(eventName, (event) => {
     pool.push(event.detail);
 
+    /* Set off the timeout after first caught event */
     if (!hasTimerStarted) {
       hasTimerStarted = true;
-      setTimeout(() => callback(pool, event), options.timeout);
+
+      setTimeout(() => {
+        callback(pool, event);
+
+        /* Empty the inner state for further accumulation */
+        pool = [];
+        hasTimerStarted = false;
+      }, options.timeout);
     }
   }, false));
 };
