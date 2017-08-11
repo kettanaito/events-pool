904b00c29d13ff3e6ba0bc778c5303bed3129e2f
Artem Zakharchenko <kettanaito@gmail.com>
2017-08-11 11:44:03 +0200

    Add Flow static type checker

--- src/index.js
+++ src/index.js
@@ -1,30 +1,42 @@
-const EventPool = (customOptions) => {
-  const options = Object.assign({}, {
-    events: '',
-    callback: () => ({}),
-    timeout: 15,
-  }, customOptions);
+/* @flow */
+type EventPoolOptions = {
+  events: Array<string> | string,
+  callback: Function,
+  timeout: number
+}
 
-  const { events } = options;
+/* Default options */
+const defaultOptions: EventPoolOptions = {
+  events: [],
+  timeout: 20
+};
 
+const EventPool = (options: EventPoolOptions) => {
+  const { events, timeout, callback } = { ...defaultOptions, ...options };
+
+  /* Ensure event names are always in an Array */
   const eventsList = Array.isArray(events) ? events : [events];
+
+  /* Declare internal state variables */
   let pool = [];
   let hasTimerStarted = false;
 
+  /* Loop through each event name and attach a proper event listener to it */
   eventsList.forEach(eventName => document.addEventListener(eventName, (event) => {
     pool.push(event.detail);
 
-    /* Set off the timeout after first caught event */
     if (!hasTimerStarted) {
+      /* Set off the timeout after first caught event */
       hasTimerStarted = true;
 
+      /* Set the timeout to execute a callback function after it is reached */
       setTimeout(() => {
         callback(pool, event);
 
-        /* Empty the inner state for further accumulation */
+        /* Reset the inner state for the further accumulation */
         pool = [];
         hasTimerStarted = false;
-      }, options.timeout);
+      }, timeout);
     }
   }, false));
 };
