be9ac44baaf55a8d59c0ab7dd65fa23f16ad1ff6
Artem Zakharchenko <kettanaito@gmail.com>
2017-08-12 10:44:00 +0200

    Flow: Improve code coverage

--- src/index.js
+++ src/index.js
@@ -1,29 +1,41 @@
 /* @flow */
 type EventPoolOptions = {
+  /* Name(s) of events to listen to */
   events: Array<string> | string,
-  callback: Function,
-  timeout: number
+
+  /* Function to call once timeout is reached */
+  callback: (pool: Array<mixed>, event?: CustomEvent | Event) => void,
+
+  /* Timeout (ms) before the callback / next aggregation */
+  timeout: number,
+
+  /* Aggregate the timeout by its value once next event is caught */
+  aggregate: boolean
 }
 
 /* Default options */
 const defaultOptions: EventPoolOptions = {
-  events: [],
-  timeout: 20
+  events: '',
+  callback: pool => console.log('Accumulated pool:', pool),
+  timeout: 20,
+  aggregate: false
 };
 
 const EventPool = (options: EventPoolOptions) => {
+  /* Combine and destruct the options */
   const { events, timeout, callback } = { ...defaultOptions, ...options };
 
   /* Ensure event names are always in an Array */
   const eventsList = Array.isArray(events) ? events : [events];
 
   /* Declare internal state variables */
-  let pool = [];
+  let pool: Array<any> = [];
   let hasTimerStarted = false;
 
   /* Loop through each event name and attach a proper event listener to it */
-  eventsList.forEach(eventName => document.addEventListener(eventName, (event) => {
-    pool.push(event.detail);
+  eventsList.forEach(eventName => document.addEventListener(eventName, (event: CustomEvent | Event) => {
+    /* Aggregate custom event details or general event instances */
+    pool.push((event instanceof CustomEvent) ? event.detail : event);
 
     if (!hasTimerStarted) {
       /* Set off the timeout after first caught event */
