65243fe6f4bc707a26ad17d26e8301dcfd9d6b1f
Artem Zakharchenko <kettanaito@gmail.com>
2017-08-15 17:14:58 +0200

    Add tests for multiple events

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,3 +1,7 @@
+function createElement (elementName) {
+  return document.body.appendChild(document.createElement(elementName));
+}
+
 describe('Technical:', () => {
   it('Can be imported', () => {
     return expect(EventPool).to.not.be.undefined;
@@ -5,25 +9,36 @@ describe('Technical:', () => {
 });
 
 describe('Basics:', () => {
-  it('Accept custom EventTarget', (done) => {
-    const boxDOMElement = document.getElementById('box');
-
-    new EventPool({
-      events: 'click',
-      eventTarget: boxDOMElement,
+  it('Dispatch events from document by defaul', (done) => {
+    EventPool({
+      events: 'documentEvent',
       callback() {
         return done();
       }
     });
 
-    boxDOMElement.click();
+    document.dispatchEvent(new CustomEvent('documentEvent'));
+  });
+
+  it('Dispatch events from custom eventTarget', (done) => {
+    const eventTarget = createElement('eventtarget');
+
+    EventPool({
+      events: 'click',
+      eventTarget,
+      callback() {
+        return done();
+      }
+    });
+
+    eventTarget.click();
   });
 
   it('Single event: Catch correctly', (done) => {
-    new EventPool({
+    EventPool({
       events: 'customEvent',
       callback() {
-        done();
+        return done();
       }
     });
 
@@ -31,8 +46,8 @@ describe('Basics:', () => {
     document.dispatchEvent(customEvent);
   });
 
-  it('Single event: Receive CustomEvent.detail data', (done) => {
-    new EventPool({
+  it('Single event: Receive data successfully', (done) => {
+    EventPool({
       events: 'onApplePick',
       callback(pool) {
         expect(pool).to.not.be.undefined;
@@ -45,5 +60,40 @@ describe('Basics:', () => {
       detail: { apples: 4 }
     });
     document.dispatchEvent(customEvent);
+  });
+
+  it('Multiple events: Catch correctly', (done) => {
+    EventPool({
+      events: 'multipleEvent_1',
+      callback(pool) {
+        expect(pool.length).to.equal(2);
+        return done();
+      }
+    });
+
+    const eventEntity = new CustomEvent('multipleEvent_1');
+
+    document.dispatchEvent(eventEntity);
+    document.dispatchEvent(eventEntity);
+  });
+
+  it('Multiple events: Aggregate data successfully', (done) => {
+    EventPool({
+      events: 'multipleEvent_2',
+      callback(pool) {
+        const sum = pool.reduce((number, entry) => number += entry.number, 0);
+
+        expect(pool.length).to.equal(2);
+        expect(sum).to.equal(10);
+        return done();
+      }
+    });
+
+    document.dispatchEvent(new CustomEvent('multipleEvent_2', {
+      detail: { number: 2 }
+    }));
+    document.dispatchEvent(new CustomEvent('multipleEvent_2', {
+      detail: { number: 8 }
+    }));
   });
 });
